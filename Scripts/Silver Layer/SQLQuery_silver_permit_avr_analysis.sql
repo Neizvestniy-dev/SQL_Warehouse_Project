5b91a1a72114666bdf101621faa7676066587e69
Neizvestniy-dev <olegkhilchenko@outlook.com>
2025-04-25 13:12:22 -0400

    Update SQLQuery_silver_permit_avr_analysis.sql

--- Scripts/Silver Layer/SQLQuery_silver_permit_avr_analysis.sql
+++ Scripts/Silver Layer/SQLQuery_silver_permit_avr_analysis.sql
@@ -1,6 +1,6 @@
 ---Add new column that calculates the years passed between application date and completion datee---
 
----test and drop---
+---Test and drop---
 if col_length ('silver.combined_permit_data', 'date_difference') is not null
 	alter table [silver].[combined_permit_data] drop column [date_difference];
 ---Create new column---
@@ -9,12 +9,12 @@ alter table [silver].[combined_permit_data]
 ---Populate new column---
 UPDATE [silver].[combined_permit_data]
 set date_difference = datediff(year,application_date, isnull(complete_date, getdate())) 
----Delete empry rows---
+---Delete empty rows---
 delete from [silver].[combined_permit_data]
 	where  [permit_num] is null
 ---View the whole table---
 select * from [silver].[combined_permit_data]
----View the averages for each type---
+---View the averages for each type of permit---
 select [permit_type], AVG([date_difference]) as average from [silver].[combined_permit_data]
 group by [permit_type]
 
