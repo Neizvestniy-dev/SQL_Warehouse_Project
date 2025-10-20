b4f441232d9cf0135100ea5a9153b6e1dd1a2069
Oleg K <olegkhilchenko@outlook.com>
2025-10-20 14:25:59 -0400

    Add date_difference column and update queries
    This script adds a new column to calculate the years passed between application and completion dates, populates it, and provides various average calculations based on permit types and application dates.

--- Scripts/Silver Layer/SQLQuery_silver_permit_avr_analysis.sql
+++ Scripts/Silver Layer/SQLQuery_silver_permit_avr_analysis.sql
@@ -1,20 +1,41 @@
----Add new column that calculates the years passed between application date and completion datee---
-
----Test and drop---
+use DataWarehouse
+--- Check for existance ---
 if col_length ('silver.combined_permit_data', 'date_difference') is not null
-	alter table [silver].[combined_permit_data] drop column [date_difference];
----Create new column---
+	alter table [silver].[combined_permit_data] drop column [date_difference]
+--- Add new column date_difference ---
 alter table [silver].[combined_permit_data]
 	add date_difference int
----Populate new column---
+--- Populate ne column date_diiference that calculate time between application_date and issue_date ---
 UPDATE [silver].[combined_permit_data]
-set date_difference = datediff(year,application_date, isnull(complete_date, getdate())) 
----Delete empty rows---
-delete from [silver].[combined_permit_data]
-	where  [permit_num] is null
+set date_difference = datediff(DAY,application_date, issue_date) 
 ---View the whole table---
 select * from [silver].[combined_permit_data]
----View the averages for each type of permit---
-select [permit_type], AVG([date_difference]) as average from [silver].[combined_permit_data]
-group by [permit_type]
-
+---View the averages for each type of permit in different time brackets---
+select permit_type,
+AVG(case when year (application_date) <= 2008 then date_difference end) as average_before_2008, 
+AVG(case when year (application_date) < 2019 and year(application_date) > 2008 then date_difference end) as average_2008to2019, 
+AVG(case when year (application_date) >= 2019 and year(application_date) < 2024 then date_difference end) as average_after_2019to2024,
+AVG(case when year (application_date) > 2024 then date_difference end) as average_after_2024
+from silver.combined_permit_data
+where date_difference is not null 
+group by permit_type
+--- Check obnormalitites with Site Inspections -- 
+select * from silver.combined_permit_data
+where permit_type = 'Site Inspection(Scarborough)'
+--- Qualitative check---
+SELECT COUNT(permit_type) 
+FROM silver.combined_permit_data
+WHERE permit_type = 'Residential Building Permit'
+UNION ALL
+SELECT COUNT(abp_permit_type) 
+FROM bronze.active_building_permits
+WHERE abp_permit_type = 'Residential Building Permit'
+UNION ALL
+SELECT COUNT(cbp_permit_type)
+FROM bronze.cleared_building_permits
+WHERE cbp_permit_type = 'Residential Building Permit'
+---Inspect Residential Building Permit ---
+select * 
+from silver.combined_permit_data
+where permit_type = 'Residential Building Permit'
+order by application_date desc
